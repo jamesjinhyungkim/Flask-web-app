34ae77d9d6bf698d1b29aa04f55398e9de5fea90
jamesjinhyungkim <91632663+jamesjinhyungkim@users.noreply.github.com>
2021-10-05 12:39:56 -0700

    Chapter 2 Templates and For Loops

--- app/routes.py
+++ app/routes.py
@@ -5,5 +5,18 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'James'}
-    return render_template('index.html', title='Home', user=user)
+    #username
+    posts = [
+         {
+              'author': {'username': 'John'},
+              'body': 'Beautiful day in West Hollywood!'
+              },
+              {
+                   'author': {'username': 'Susan'},
+                   'body': 'The Dodgers will beat the Cardinals on wednesday!'
+                   }
+        }
+     ]
+     #Create fake posts & users
+     return render_template('index.html', title='Home', user=user, posts=posts)     
     
