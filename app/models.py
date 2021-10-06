baa5377026c6f84a62a4db011d7a227c233dedc0
jamesjinhyungkim <91632663+jamesjinhyungkim@users.noreply.github.com>
2021-10-06 12:27:03 -0700

    Chapter 4 with SQLALCHEMY_URI Error

---
+++ app/models.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from app import db
+
+#Create user db model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(64), index = True, unique = True)
+    email = db.Column(db.String(120), index = True, unique = True)
+    password_hash = db.Column(db.String(128))
+    
+    #Printing the object of the User class
+    def __repr__(self):
+        return '<User {}.>'.format(self.username)
+
+#create database relationships to link post to author      
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index = True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+    def __repr__(self):
+        return '<Post{}>'.format(self.body)
